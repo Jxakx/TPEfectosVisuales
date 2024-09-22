0fdf36ec30ed884cba6a65a1ebead3ae048813c8
Joaco <lopezjoaquin488@gmail.com>
2024-09-22 13:04:16 -0300

    asdsa

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 10f;
+    public float rotationSpeed = 100f; 
 
     void Update()
     {
-        
+        // Movimiento de la cámara (A, D, W, S)
         float moveHorizontal = Input.GetAxis("Horizontal"); // A y D
         float moveVertical = Input.GetAxis("Vertical"); // W y S
 
-        
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-
-        
         transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+
+        // Rotación de la cámara (Q, E)
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
     }
 }
