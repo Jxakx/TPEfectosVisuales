b76d1f86063805345423e49f08a835cf4c9ef50f
Joaco <lopezjoaquin488@gmail.com>
2024-10-18 12:42:41 -0300

    Revert "cositas"
    This reverts commit 0b5765b1cb7c6017b77956a28fc9f4826a814a1a.

--- Assets/Shaders/ColdEffectPostProcess.cs
+++ Assets/Shaders/ColdEffectPostProcess.cs
@@ -1,58 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 
-public class ColdEffectPostProcess : ScriptableRendererFeature
+[ExecuteInEditMode]
+public class ColdEffectPostProcess : MonoBehaviour
 {
-    class CustomRenderPass : ScriptableRenderPass
-    {
-        public Material postProcessMaterial;
-
-        private RenderTargetIdentifier currentTarget;
-
-        public void Setup(RenderTargetIdentifier source)
-        {
-            this.currentTarget = source;
-        }
-
-        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-        {
-            if (postProcessMaterial == null)
-            {
-                Debug.LogWarning("No post-process material assigned.");
-                return;
-            }
-
-            CommandBuffer cmd = CommandBufferPool.Get("ColdEffectPostProcess");
-
-            // Apply the post-processing material to the current render target
-            Blit(cmd, currentTarget, currentTarget, postProcessMaterial);
-
-            context.ExecuteCommandBuffer(cmd);
-            CommandBufferPool.Release(cmd);
-        }
-    }
-
-    CustomRenderPass m_ScriptablePass;
     public Material postProcessMaterial;
 
-    public override void Create()
-    {
-        m_ScriptablePass = new CustomRenderPass();
-
-        // Configura el paso de renderización para que se ejecute después de todo el renderizado normal.
-        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
-    }
-
-    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         if (postProcessMaterial != null)
         {
-            m_ScriptablePass.Setup(renderer.cameraColorTarget);
-            m_ScriptablePass.postProcessMaterial = postProcessMaterial;
-            renderer.EnqueuePass(m_ScriptablePass);
+            Graphics.Blit(source, destination, postProcessMaterial);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
         }
     }
 }
