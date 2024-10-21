23299aac3033e7da396b9aa99bcba183d4e1b474
Joaco <lopezjoaquin488@gmail.com>
2024-10-21 20:00:49 -0300

    cod

---
+++ Assets/PP_Controls.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PP_Controls : MonoBehaviour
+{
+    [SerializeField] private Material _PPFreezeMaterial;
+    //[SerializeField] private Material _PPOutlineMaterial;
+    [SerializeField] private bool _IsOn;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            TurnoOnOff();
+        }
+       
+    }
+
+    public void TurnoOnOff()
+    {
+        if (_IsOn)
+        {
+            _PPFreezeMaterial.SetFloat("_FreezeAmount", 0f);
+            _IsOn = false;
+        }
+        else
+        {
+            _PPFreezeMaterial.SetFloat("_FreezeAmount", 1);
+            _IsOn = true;
+        }
+    }
+}
